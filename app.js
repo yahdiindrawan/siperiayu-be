018175576a870290eca65d7fe8732da25345d681
Yahdi Indrawan <yahdiindrawan11@gmail.com>
2024-06-16 01:04:00 +0700

    connect express and vue

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const port = 3000
 
+// Middleware
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send('SIPERI-AYU BE')
+    res.status(200).json({
+        message: "Message dari endpoint express"
+    })
 })
 
 app.listen(port, () => {
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Test 123</h1>
+  <h1>{{dataMessage?.message}}</h1>
 </template>
+
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const dataMessage = ref(null)
+
+const getMessage = async () => {
+  try {
+    const { data } = await axios.get("http://localhost:3000/")
+    dataMessage.value = data;
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+onMounted(() => {
+  getMessage();
+});
+</script>
